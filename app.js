e0f42a569ede842962a84112d53637096714357d
herr_robo <walkintalkinrobot@gmail.com>
2022-07-19 17:40:49 -0500

    Next round of topics covered

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
-// npm - global command, comes with node
-// npm --version
+// require('./event-loop-examples/1-read-file');
 
-// local dependency - use it only in this particular project
-// npm i <packageName>
+//require('./event-loop-examples/2-setTimeout');
 
-// global dependency - use it in any project
-// npm install -g <packageName>
-// sudo install -g <packageName> (mac)
+//require('./event-loop-examples/3-setInterval');
 
-// package.json - manifest file (stores important info about project/package)
-// manual approach (create package.json in the root, create properties)
-// npm init (step by step, press enter for help)
-// npm init -y (everything default)
-
-const _ = require('lodash');
-
-const items = [1, [2, [3, [4]]]];
-
-const newItems = _.flattenDeep(items);
-
-console.log(newItems);
+require('./event-loop-examples/4-server');
