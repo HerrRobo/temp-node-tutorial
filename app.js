260435cd31506a20f789abe0b9d6104263076ce9
herr_robo <walkintalkinrobot@gmail.com>
2022-07-20 00:49:35 -0500

    Another round of example learning

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
-// require('./event-loop-examples/1-read-file');
+const { readFile, writeFile } = require('fs').promises
 
-//require('./event-loop-examples/2-setTimeout');
+///const util = require('util');
+///const readFilePromise = util.promisify(readFile);
+///const writeFilePromise = util.promisify(writeFile);
 
-//require('./event-loop-examples/3-setInterval');
 
-require('./event-loop-examples/4-server');
+
+
+const start = async() => {
+    try {
+        const first = await readFile('./content/first.txt', 'utf8', );
+        console.log(first);
+        const second = await readFile('./content/second.txt', 'utf8', );
+        console.log(second);
+        await writeFile('./content/result-mind-grenade.txt', `This is awesome : ${first} -- ${second}`, {flag: 'a'}); 
+    }
+    catch (error) {
+        console.log(error);
+    }
+} 
+
+start();
+
+/* const getText = (path) => {
+    return new Promise((resolve, reject) => {
+
+        readFile(path, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(data);
+            }
+        })
+    })
+} */
+
+
+
+//getText('./content/first.txt')
+//    .then(result => console.log(result))
+//    .catch((err) => console.log(err));
